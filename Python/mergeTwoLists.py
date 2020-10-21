9164469abb7d19a0c66dd9e995af4b41a064dfdd
Cameron Lane <cameron.lane@me.com>
2020-10-21 13:38:16 -0700

    97% Faster, 100% less memory

---
+++ Python/mergeTwoLists.py
@@ -0,0 +1,24 @@
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        tail = ListNode(0)
+        head = tail
+        
+        while l1 and l2:
+            if l1.val <= l2.val:
+                head.next = l1
+                l1 = l1.next
+            else:
+                head.next = l2
+                l2 = l2.next
+            head = head.next
+        if l1:
+            head.next = l1
+        elif l2:
+            head.next = l2
+
+        return tail.next
