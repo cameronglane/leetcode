626df422fd55f989110c5179532453a4174fd1df
Cameron Lane <cameron.lane@me.com>
2020-10-22 03:13:39 -0700

    98.58% faster, 66.46% less memory

--- Python/intervalIntersection.py
+++ Python/intervalIntersection.py
@@ -1,19 +1,15 @@
 class Solution:
     def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
         return_list = []
+        a_it, b_it = 0, 0
 
-        while A and B:
-            print("\nComparing {} and {}".format(A[0], B[0]))
-            if A[0][0] >= B[0][0]:
-                if B[0][1] >= A[0][0]:
-                    return_list.append([A[0][0], min(A[0][1], B[0][1])])
-                    print("Adding {}".format([A[0][0], min(A[0][1], B[0][1])]))
-                print("Deleting {} from B".format(B[0]))
-                B.pop(0)
+        while a_it < len(A) and b_it < len(B):
+            x, y = max(A[a_it][0], B[b_it][0]), min(A[a_it][1], B[b_it][1])
+
+            if x <= y:
+                return_list.append([x,y])
+            if A[a_it][1] > B[b_it][1]:
+                b_it += 1
             else:
-                if A[0][1] >= B[0][0]:
-                    return_list.append([B[0][0],  min(A[0][1], B[0][1])])
-                    print("Adding {}".format([B[0][0], min(A[0][1], B[0][1])]))
-                print("Deleting {} from A".format(A[0]))
-                A.pop(0)
+                a_it += 1
         return return_list
