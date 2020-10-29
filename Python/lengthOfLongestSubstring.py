9766c703fc2080121dedd4dfef0c76d4fa4eadd6
Cameron Lane <cameron.lane@me.com>
2020-10-28 23:50:50 -0700

    95.33% faster, 100% less memory

--- Python/lengthOfLongestSubstring.py
+++ Python/lengthOfLongestSubstring.py
@@ -1,16 +1,10 @@
-from collections import deque
-import itertools
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        longest_string = 0
-        queue = deque([])
-        
-        for char in s:
-            if queue.count(char) > 0:
-                for _ in itertools.repeat(None,queue.index(char) + 1):
-                    queue.popleft()
-            queue.append(char)
-            if len(queue) > longest_string:
-                longest_string = len(queue)
-        return longest_string
+        last, res, st = {}, 0, 0
+        for i, v in enumerate(s):
+            if v not in last or last[v] < st:
+                res = max(res, i - st + 1)
+            else:
+                st = last[v] + 1
+            last[v] = i
+        return res
