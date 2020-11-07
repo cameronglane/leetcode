61d737541cc06eae8b7f1aea2f7ff2e8ca3d3958
Cameron Lane <cameron.lane@me.com>
2020-11-06 22:53:57 -0800

    An alternate version

--- Python/parentheses.py
+++ Python/parentheses.py
@@ -1,6 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        char_map = {")": "(", "]": "[", "}": "{"}
+
+        for char in s:
+            if char in char_map:
+                top = stack.pop() if stack else 'empty'
+
+                if char_map[char] != top:
+                    return False
+            else:
+                stack.append(char)
+    
+        return not stack
+
+#Alt:
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        stack = []
         return_chars = ['}', ')', ']']
         for char in s:
             if return_chars.count(char):
